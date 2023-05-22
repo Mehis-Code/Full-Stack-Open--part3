2e309107bd7fbb96c0ecd9a76696ae7bcee65c98
Mehis <vikkek@gmail.com>
2023-05-22 22:16:37 +0300

    testausta

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ const requestLogger = (request, response, next) => {
   const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
   }
-  
+  const cors = require('cors')
+
+  app.use(cors())
   app.use(express.json())
   app.use(requestLogger)
   morgan.token('cont', function (req, res) { return JSON.stringify(req.body) })
@@ -107,7 +109,7 @@ app.delete('/api/persons/:id', (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
