07a85c0a5d0e512de473135736bfbb07bc90886b
Mehis <vikkek@gmail.com>
2023-06-17 01:53:26 +0300

    3.18

--- index.js
+++ index.js
@@ -2,76 +2,68 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
-const morgan = require('morgan')
-const Person = require('./models/person')
-const url = process.env.MONGODB_URI
+const morgan = require('morgan');
+const Person = require('./models/person');
+const url = process.env.MONGODB_URI;
 
-console.log('connecting to', url)
-
-mongoose.connect(url)
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+console.log('connecting to', url);
 
 const requestLogger = (request, response, next) => {
-    console.log('Method:', request.method)
-    console.log('Path:  ', request.path)
-    console.log('Body:  ', request.body)
-    console.log('---')
-    next()
+  console.log('Method:', request.method);
+  console.log('Path:  ', request.path);
+  console.log('Body:  ', request.body);
+  console.log('---');
+  next();
+};
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
   }
-  
-  const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
-  }
-  const cors = require('cors')
-  app.use(express.static('build'))
-  app.use(cors())
-  app.use(express.json())
-  app.use(requestLogger)
-  morgan.token('cont', function (req, res) { return JSON.stringify(req.body) })
-  app.use(morgan(':method :url :status :res[content-length] - :response-time ms :cont'))
-  
+  next(error);
+};
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+
+const cors = require('cors');
+app.use(express.static('build'));
+app.use(cors());
+app.use(express.json());
+app.use(requestLogger);
+morgan.token('cont', function (req, res) { return JSON.stringify(req.body); });
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :cont'));
+
 let persons = [];
 
-app.get('/api/persons', (request, response) => {
-  Person.find({}).then(persons => {
-    response.json(persons);
-  }).catch(error => {
-    console.log(error);
-    response.status(500).send({ error: 'Error retrieving persons from the database.' });
-  });
-});
-
-app.post('/api/persons', (request, response) => {
-      const body = request.body
-      if (!body.name || !body.number) {
-        return response.status(400).json({ error: 'name or number missing' });
-      }
-    
-      const person = new Person({
-        name: body.name,
-        number: body.number,
-      });
-      person.save()
-      .then(savedPerson => {
-        response.json(savedPerson);
-      })
-      .catch(error => {
-        console.log(error);
-        response.status(500).send({ error: 'Error saving person to the database.' });
-      });
+app.get('/api/persons', (request, response, next) => {
+  Person.find({})
+    .then(persons => {
+      response.json(persons);
     })
-
-
-app.get('/', (req, res) => {
-  res.send(persons);
+    .catch(error => next(error));
 });
 
-app.get('/api/persons/:id', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body;
+  if (!body.name || !body.number) {
+    return response.status(400).json({ error: 'name or number missing' });
+  }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  });
+  person.save()
+    .then(savedPerson => {
+      response.json(savedPerson);
+    })
+    .catch(error => next(error));
+});
+
+app.get('/api/persons/:id', (request, response, next) => {
   const id = request.params.id;
   Person.findById(id)
     .then(person => {
@@ -81,22 +73,48 @@ app.get('/api/persons/:id', (request, response) => {
         response.status(404).end();
       }
     })
-    .catch(error => {
-      console.log(error);
-      response.status(500).send({ error: 'Error retrieving person from the database.' });
-    });
+    .catch(error => next(error));
+});
+app.get('/info', (request, response, next) => {
+  Person.countDocuments({})
+    .then(count => {
+      const info = `<p>Phonebook has info for ${count} people</p><p>${new Date()}</p>`;
+      response.send(info);
+    })
+    .catch(error => next(error));
 });
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(pers => pers.id !== id)
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end();
+    })
+    .catch(error => next(error));
+});
 
-  response.status(204).end()
-  })
+app.put('/api/persons/:id', (request, response, next) => {
+  const id = request.params.id;
+  const body = request.body;
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
 
-app.use(unknownEndpoint)
+  Person.findByIdAndUpdate(id, person, { new: true, upsert: true })
+    .then(updatedPerson => {
+      if (updatedPerson) {
+        response.json(updatedPerson);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch(error => next(error));
+});
 
-const PORT = process.env.PORT
+app.use(unknownEndpoint);
+app.use(errorHandler);
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
