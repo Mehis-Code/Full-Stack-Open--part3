4c111f8d1bb76176dc8c102f003efabbe8018987
Mehis <vikkek@gmail.com>
2023-06-17 03:09:36 +0300

    uibuild

--- models/person.js
+++ models/person.js
@@ -12,16 +12,30 @@ mongoose.connect(url).then(result => {
         console.log('error connecting to MongoDB:', error.message)})
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
-})
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+    minlength: [3, 'Name must be at least 3 characters long']
+  },
+  number: {
+    type: String,
+    required: true,
+    minlength: 8,
+    validate: {
+      validator: function (value) {
+        return /^\d{2,3}-\d{7,}$/.test(value);
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    }
+  }
+});
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
-  })
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  }
+});
 
 module.exports = mongoose.model('Person', personSchema)
